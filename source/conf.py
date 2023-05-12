d2d925f42aa7ce494135c77f764d1fca171517b7
Ashwin Naren <arihant2math@gmail.com>
2023-05-12 16:01:41 -0700

    code reformatting

--- source/conf.py
+++ source/conf.py
@@ -5,27 +5,26 @@
 import os
 import sys
 
-sys.path.insert(0, os.path.abspath("..")) # Make autodoc work
+sys.path.insert(0, os.path.abspath(".."))  # Make autodoc work
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'Autotraders'
-copyright = '2023, Ashwin Naren'
-author = 'Ashwin Naren'
-release = '0.2.0'
+project = "Autotraders"
+copyright = "2023, Ashwin Naren"
+author = "Ashwin Naren"
+release = "0.2.0"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
 extensions = ["sphinx.ext.autodoc", "sphinx.ext.coverage", "sphinx.ext.napoleon"]
 
-templates_path = ['_templates']
+templates_path = ["_templates"]
 exclude_patterns = []
-
 
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = "furo"
-html_static_path = ['_static']
+html_static_path = ["_static"]
