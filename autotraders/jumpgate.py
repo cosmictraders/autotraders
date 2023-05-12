47371543343d6c3da484992ba9c852ca0e75be49
Ashwin Naren <arihant2math@gmail.com>
2023-05-12 16:01:34 -0700

    added jumpgate support

---
+++ autotraders/jumpgate.py
@@ -0,0 +1,23 @@
+class JumpGate:
+    def __init__(self, waypoint: str, session, update=True):
+        self.location = waypoint
+        self.session = session
+        self.faction_symbol = ""
+        if update:
+            self.update()
+
+    def update(self, data: dict = None):
+        if data is None:
+            split = self.location.split("-")
+            system_symbol = split[0] + "-" + split[1]
+            waypoint_symbol = self.location
+            data = self.session.get(
+                "https://api.spacetraders.io/v2/systems/"
+                + system_symbol
+                + "/waypoints/"
+                + waypoint_symbol
+                + "/jump-gate"
+            ).json()["data"]
+        self.jump_range = data["jumpRange"]
+        if "factionSymbol" in data:
+            self.faction_symbol = data["factionSymbol"]
