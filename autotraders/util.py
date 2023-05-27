c6b7f1ca76fcb88d442ac433e853a6abe1446a8f
Ashwin Naren <arihant2math@gmail.com>
2023-05-27 14:24:49 -0700

    travel formulas

--- autotraders/util.py
+++ autotraders/util.py
@@ -15,3 +15,27 @@ def parse_time(time: str) -> datetime:
                 d = datetime.fromisoformat(time)
         d = d.replace(tzinfo=timezone.utc)
         return d
+
+
+def travel_fuel(distance, mode):
+    if mode == "CRUISE":
+        return distance
+    elif mode == "DRIFT":
+        return 1
+    elif mode == "BURN":
+        return 2 * distance
+    elif mode == "STEALTH":
+        return distance
+
+
+def travel_time(distance, ship_speed, mode):
+    if mode == "CRUISE":
+        return 15 + 10 * distance / ship_speed
+    elif mode == "DRIFT":
+        return 15 + 100 * distance / ship_speed
+    elif mode == "BURN":
+        return 15 + 5 * distance / ship_speed
+    elif mode == "STEALTH":
+        return 15 + 20 * distance / ship_speed
+
+
