05c48e01217da5d3aefe340c2daa724dd5b80934
Ashwin Naren <arihant2math@gmail.com>
2023-05-27 22:46:07 -0700

    added market transaction

--- autotraders/transaction.py
+++ autotraders/transaction.py
@@ -1,10 +1,24 @@
+from autotraders.shared_models.map_symbol import MapSymbol
 from autotraders.util import parse_time
 
 
 class ShipyardTransaction:
     def __init__(self, data):
         self.credits = data["price"]
-        self.waypoint_symbol = data["waypointSymbol"]
+        self.waypoint_symbol = MapSymbol(data["waypointSymbol"])
         self.ship_symbol = data["shipSymbol"]
         self.agent_symbol = data["agent_symbol"]
+        self.timestamp = parse_time(data["timestamp"])
+
+
+class MarketTransaction:
+    def __init__(self, data):
+        self.waypoint_symbol = MapSymbol(data["waypointSymbol"])
+        self.ship_symbol = data["shipSymbol"]
+
+        self.trade_symbol = data["tradeSymbol"]
+        self.transaction_type = data["type"]
+        self.units = data["units"]
+        self.price_per_unit = data["pricePerUnit"]
+        self.total_price = data["totalPrice"]
         self.timestamp = parse_time(data["timestamp"])
