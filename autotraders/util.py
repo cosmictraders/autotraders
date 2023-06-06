ffc9446aac649ab82b0edb53928b03e1becd9723
Ashwin Naren <arihant2math@gmail.com>
2023-06-05 19:32:20 -0700

    added ship states

--- autotraders/util.py
+++ autotraders/util.py
@@ -1,6 +1,8 @@
 import sys
 from datetime import datetime, timezone
 
+from autotraders.ship.states import FlightMode
+
 
 def parse_time(time: str) -> datetime:
     if sys.version_info.minor >= 11:
@@ -18,22 +20,22 @@ def parse_time(time: str) -> datetime:
 
 
 def travel_fuel(distance, mode):
-    if mode == "CRUISE":
+    if mode == FlightMode.CRUISE:
         return distance
-    elif mode == "DRIFT":
+    elif mode == FlightMode.DRIFT:
         return 1
-    elif mode == "BURN":
+    elif mode == FlightMode.BURN:
         return 2 * distance
-    elif mode == "STEALTH":
+    elif mode == FlightMode.STEALTH:
         return distance
 
 
 def travel_time(distance, ship_speed, mode):
-    if mode == "CRUISE":
+    if mode == FlightMode.CRUISE:
         return 15 + 10 * distance / ship_speed
-    elif mode == "DRIFT":
+    elif mode == FlightMode.DRIFT:
         return 15 + 100 * distance / ship_speed
-    elif mode == "BURN":
+    elif mode == FlightMode.BURN:
         return 15 + 5 * distance / ship_speed
-    elif mode == "STEALTH":
+    elif mode == FlightMode.STEALTH:
         return 15 + 20 * distance / ship_speed
