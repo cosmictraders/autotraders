34c9cfdac2a7be9cb840f1c4385157cc5313567d
Ashwin Naren <arihant2math@gmail.com>
2023-07-12 19:15:12 -0700

    added agent updates

--- autotraders/agent.py
+++ autotraders/agent.py
@@ -1,3 +1,6 @@
+from typing import Optional
+
+from autotraders.error import SpaceTradersException
 from autotraders.faction.contract import Contract
 from autotraders.paginated_list import PaginatedList
 from autotraders.shared_models.map_symbol import MapSymbol
@@ -7,25 +10,53 @@ from autotraders.ship import Ship
 
 
 class Agent(SpaceTradersEntity):
-    contracts: PaginatedList
+    contracts: Optional[PaginatedList]
     starting_faction: str
     symbol: str
     account_id: str
     credits: int
-    ships: PaginatedList
+    ship_count: int
+    ships: Optional[PaginatedList]
     headquarters: MapSymbol
 
-    def __init__(self, session: AutoTradersSession, data=None):
-        super().__init__(session, "my/agent", data)
+    def __init__(self, session: AutoTradersSession, symbol=None, data=None):
+        if symbol is None:
+            super().__init__(session, "my/agent", data)
+        else:
+            super().__init__(session, "agents/" + symbol, data)
 
     def update(self, data=None):
         """Uses 3 API requests to get all agent details"""
         if data is None:
             data = self.get()["data"]
-        self.account_id = data["accountId"]
+        if "account_id" in data:
+            self.account_id = data["accountId"]
+            self.ships = Ship.all(self.session)
+            self.contracts = Contract.all(self.session)
         self.symbol = data["symbol"]
         self.headquarters = MapSymbol(data["headquarters"])
         self.credits = data["credits"]
         self.starting_faction = data["startingFaction"]
-        self.ships = Ship.all(self.session)
-        self.contracts = Contract.all(self.session)
+        self.ship_count = data["shipCount"]
+
+    @staticmethod
+    def all(session, page: int = 1) -> PaginatedList:
+        def paginated_func(p, num_per_page):
+            r = session.get(
+                session.base_url
+                + "agents?limit="
+                + str(num_per_page)
+                + "&page="
+                + str(p)
+            )
+            j = r.json()
+            if "error" in j:
+                raise SpaceTradersException(j["error"], r.status_code)
+            agents = []
+            for agent in j["data"]:
+                a = Agent(session, agent["symbol"], agent)
+                agents.append(a)
+            return agents, r.json()["meta"]["total"]
+
+        return PaginatedList(paginated_func, page)
+
