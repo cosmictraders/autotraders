d0b0103e3828799e90472c22c3d6b85984ae8767
Ashwin Naren <arihant2math@gmail.com>
2023-07-17 21:08:10 -0700

    added agent registration method

--- autotraders/__init__.py
+++ autotraders/__init__.py
@@ -1,6 +1,20 @@
 """A spacetraders api"""
+import requests
 
 from autotraders.session import AutoTradersSession  # noqa F401
 from autotraders.status import get_status  # noqa F401
 
 __version__ = "1.7.1"
+
+
+def register_agent(
+    symbol: str, faction: str, email=None, url="https://api.spacetraders.io/v2/"
+):
+    requests.post(
+        url + "register",
+        data={
+            "faction": faction.upper(),
+            "symbol": symbol,
+            "email": email,
+        },
+    )
