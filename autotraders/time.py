66bdb4d57e1b562f7f4c53cd3c6d69445b26f03f
Ashwin Naren <arihant2math@gmail.com>
2023-10-24 14:43:17 -0700

    more docs

--- autotraders/time.py
+++ autotraders/time.py
@@ -4,7 +4,8 @@ from datetime import datetime, timezone
 
 def parse_time(time: str) -> datetime:
     """
-    :param time: A string representing a timestamp in either ISO format (YYYY-MM-DDTHH:MM:SS.SSSZ) or a custom format (%Y-%m-%dT%H:%M:%S.%fZ).
+    :param time: A string representing a timestamp in either ISO format (YYYY-MM-DDTHH:MM:SS.SSSZ)
+                 or a custom format (%Y-%m-%dT%H:%M:%S.%fZ).
 
     :return: A datetime object with the parsed timestamp.
 
@@ -13,13 +14,13 @@ def parse_time(time: str) -> datetime:
     """
     if sys.version_info.minor >= 11:
         return datetime.fromisoformat(time)
-    else:
+    else:  # Workaround for python before 3.11
         try:
             d = datetime.strptime(time, "%Y-%m-%dT%H:%M:%S.%fZ")
         except ValueError:
             try:
                 d = datetime.strptime(time, "%Y-%m-%dT%H:%M:%SZ")
-            except ValueError:
+            except ValueError:  # Workaround for stoplight
                 d = datetime.fromisoformat(time)
-        d = d.replace(tzinfo=timezone.utc)
+        d = d.replace(tzinfo=timezone.utc)  # Makes the time timezone aware, so it is consistent with python 3.11+
         return d
